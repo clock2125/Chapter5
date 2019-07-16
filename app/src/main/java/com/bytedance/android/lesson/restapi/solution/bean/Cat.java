fbaa65238c84d0d5a2e841942397a51a001dcf5a
clock2125 <zhongsang1998@outlook.com>
2019-07-16 20:54:16 +0800

    Chapter5 homework

---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/Cat.java
@@ -0,0 +1,54 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.17 18:08
+ */
+public class Cat {
+
+    //  Implement your Cat Bean here according to the response json
+
+    @SerializedName
+   ("id") private String id;
+    @SerializedName("url")
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @SerializedName("width") private int width;
+
+    @SerializedName("height") private int height;
+
+}
---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/PostVideoResponse.java
@@ -0,0 +1,21 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.18 17:53
+ */
+public class PostVideoResponse {
+
+    //  Implement your PostVideoResponse Bean here according to the response json
+    @SerializedName("url") private String url;
+    @SerializedName("success") private boolean success;
+
+    public String getUrl(){
+        return url;
+    }
+    public boolean getSuccess() {
+        return success;
+    }
+}
